87f57f07cef1889cdde91a680a9049602b5fd4b2
liufa <mataswork@gmail.com>
2015-12-05 12:42:45 +0000

    Added autocomoplete second part

--- AutoTrader/AutoTrader.Manager/Controllers/JsonController.cs
+++ AutoTrader/AutoTrader.Manager/Controllers/JsonController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace AutoTrader.Manager.Controllers
 {
@@ -13,8 +14,23 @@ namespace AutoTrader.Manager.Controllers
         {
             using (var db = new Data.TraderEntities())
             {
-                var model = db.Product.Where(o => o.Name == name).ToList();
+                var model = db.Product.Where(o => o.Name.Contains(name)).ToList();
                 return Json(model, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult AddProduct(string name)
+        {
+            using (var db = new Data.TraderEntities())
+            {
+                if (!db.Product.Any(o => o.Name == name))
+                {
+                    db.Product.Add(new Data.Product { Name = name });
+                    db.SaveChanges();
+                }
+                var item = db.Product.Include(o => o.ProductKeywords)
+                    .Single(o => o.Name == name);
+                return Json(item, JsonRequestBehavior.AllowGet);
             }
         }
     }
