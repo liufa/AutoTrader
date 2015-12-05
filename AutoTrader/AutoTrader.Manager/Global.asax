ba190293eb241daa4d4cd6ddb632c6534833b9b0
liufa <mataswork@gmail.com>
2015-12-05 08:30:24 +0000

    Added Management Project

---
+++ AutoTrader/AutoTrader.Manager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoTrader.Manager.MvcApplication" Language="C#" %>
---
+++ AutoTrader/AutoTrader.Manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoTrader.Manager.Startup))]
+namespace AutoTrader.Manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
