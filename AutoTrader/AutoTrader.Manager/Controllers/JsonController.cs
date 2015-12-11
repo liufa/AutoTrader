0dd86efa936a7409c0f3d6b109015ead597d1c64
liufa <mataswork@gmail.com>
2015-12-11 12:50:18 +0000

    Did mechanism to return JSON values. and mechanism to generate links for maping unmapping products to items.

--- AutoTrader/AutoTrader.Manager/Controllers/JsonController.cs
+++ AutoTrader/AutoTrader.Manager/Controllers/JsonController.cs
@@ -1,22 +1,22 @@
-﻿using System;
+﻿using AutoTrader.Data;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace AutoTrader.Manager.Controllers
 {
     public class JsonController : Controller
     {
-        // GET: Json
         public JsonResult LookupProduct(string name)
         {
+            var model = new List<Product>();
             using (var db = new Data.TraderEntities())
             {
-                var model = db.Product.Where(o => o.Name.Contains(name)).ToList();
-                return Json(model, JsonRequestBehavior.AllowGet);
+                db.Configuration.LazyLoadingEnabled = false;
+                model = db.Product.Where(o => o.Name.Contains(name)).ToList();
             }
+            return Json(model, JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult AddProduct(string name)
